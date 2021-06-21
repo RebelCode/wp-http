686046ceb965eeb97a3162f0ef7998f4677c5b16
Miguel <miguelmuscat93@gmail.com>
2021-06-21 15:54:51 +0200

    Changed tests namespace

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,5 @@
 <?php
 
-declare(strict_types=1);
+require_once __DIR__ . '/../vendor/autoload.php';
 
-(function (string $baseFile) {
-    $baseDir = dirname($baseFile);
-    $rootDir = dirname($baseDir);
-    define('ROOT_DIR', $rootDir);
-
-    error_reporting(E_ALL | E_STRICT);
-
-    require_once "$rootDir/vendor/autoload.php";
-})(__FILE__);
+WP_Mock::bootstrap();
